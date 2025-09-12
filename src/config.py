f76d839a40e4fc2522ea5941619d8e349fcdca86
MarisaDAZA <1710978922@qq.com>
2025-09-12 22:26:33 +0100

    优化

--- src/config.py
+++ src/config.py
@@ -3,6 +3,6 @@ import tomlkit
 with open('config.toml', 'r', encoding='utf-8') as f:
     config = tomlkit.load(f)
 
-config.group_list = [str(i) for i in config.group_list]
-config.private_list = [str(i) for i in config.private_list]
-config.ban_user_id = [str(i) for i in config.ban_user_id]
+config['group_list'] = [str(i) for i in config['group_list']]
+config['private_list'] = [str(i) for i in config['private_list']]
+config['ban_user_id'] = [str(i) for i in config['ban_user_id']]
